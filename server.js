66c44c301ea1523f1881973ec0836d21e547bf2d
Simon Johansson <mail@simon-johansson.com>
2015-05-29 23:20:12 +0200

    Fix to server environment setup

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 'use strict';
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 require("babel/register");
 
 var path = require('path');
--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -4,9 +4,10 @@ var path = require('path');
 var _    = require('lodash');
 
 var all = {
-  env: process.env.NODE_ENV,
+  env: process.env.NODE_ENV || 'development',
   root: path.normalize(__dirname + '/../../..'),
   port: process.env.PORT || 9000,
+  spotifyTokenRefreshRate: 3600000 // one hour
 };
 
-module.exports = _.merge(all, require(`./${process.env.NODE_ENV}.js`) || {});
+module.exports = _.merge(all, require(`./${all.env}.js`) || {});
