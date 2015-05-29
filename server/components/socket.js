a37dbb5dcb0e0693d209c9b4c9acf4da39465aaf
Simon Johansson <mail@simon-johansson.com>
2015-05-29 23:25:44 +0200

    ES6:ification of socket.io server setup

--- server/components/socket.js
+++ server/components/socket.js
@@ -1,23 +1,18 @@
 'use strict';
 
 var socketIO = require('socket.io');
-// var client = require('socket.io-client');
 var io;
 
-function bindEvents () {
-  io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
+function bindEvents (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
 }
 
-function init (server) {
-  io = socketIO(server);
-  bindEvents();
-}
-
 module.exports = {
-  init: init,
+  init(server) {
+    io = socketIO(server);
+    io.on('connection', bindEvents);
+  }
 };
