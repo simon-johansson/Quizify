fe05ddb8c48c1cd427cccf6ae88578d88d568bb9
Simon Johansson <mail@simon-johansson.com>
2015-06-09 09:17:57 +0200

    Clean up of main react component

--- src/components/QuizifyApp.js
+++ src/components/QuizifyApp.js
@@ -1,21 +1,16 @@
 'use strict';
 
 var React = require('react/addons');
-var ReactTransitionGroup = React.addons.TransitionGroup;
 
 // CSS
 require('normalize.css');
 require('../styles/main.scss');
 
-var imageURL = require('../images/yeoman.png');
-
 var QuizifyApp = React.createClass({
   render: function() {
     return (
-      <div className='main'>
-        <ReactTransitionGroup transitionName="fade">
-          <img src={imageURL} />
-        </ReactTransitionGroup>
+      <div ref="p" className='main'>
+        <h1>Quizify</h1>
       </div>
     );
   }
