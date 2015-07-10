8ecaa8fb2b7957f26d8fad58d33851c64c9a882b
Simon Johansson <mail@simon-johansson.com>
2015-07-10 23:45:41 +0200

    Redirect all faulty routes back to index

--- server/server.js
+++ server/server.js
@@ -7,4 +7,8 @@ var server = require('http').createServer(app);
 
 require('./config/express')(app);
 
+app.route('/*').get(function(req, res) {
+  res.redirect('/');
+});
+
 module.exports = server;
