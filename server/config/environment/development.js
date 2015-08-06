188531cea2cfde096027d449fee573e13ff84c94
Simon Johansson <mail@simon-johansson.com>
2015-08-06 16:48:35 +0200

    Add url property to server config

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,4 +1,7 @@
 'use strict';
 
+let ip = require('my-local-ip');
+
 module.exports = {
+  url: `http://${ip()}:${process.env.PORT || 9000}`
 };
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  port: process.env.PORT || 22654
+  port: process.env.PORT || 22654,
+  url: "http://beta.spotifyquiz.com"
 };
