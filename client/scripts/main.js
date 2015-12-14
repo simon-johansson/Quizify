027fd87d099a62f557259b51206c9f220227a7c4
Jose Granjo <jose.granjo@screeninteraction.com>
2015-12-14 23:04:31 +0100

    Re-enable fast-click

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV === 'development') {
   require('utils/DevelopmentHelpers');
 }
 
-// import fastclick from 'fastclick';
-// fastclick.attach(document.body);
+import fastclick from 'fastclick';
+fastclick.attach(document.body);
 
 import './routes';
